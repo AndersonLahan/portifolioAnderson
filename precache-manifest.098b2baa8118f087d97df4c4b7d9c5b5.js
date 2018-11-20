435d5c30c6393fa11f6787112f48fc9f9b5e4415
Anderson Lahan <andersonlahan@hotmail.com>
2018-11-19 23:41:57 -0200

    Updates

---
+++ precache-manifest.098b2baa8118f087d97df4c4b7d9c5b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6c0a36e869ca8a97986",
+    "url": "/portifolioAnderson/static/js/runtime~main.b6c0a36e.js"
+  },
+  {
+    "revision": "c7f70bed0473e6370cbf",
+    "url": "/portifolioAnderson/static/js/main.c7f70bed.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/portifolioAnderson/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "c7f70bed0473e6370cbf",
+    "url": "/portifolioAnderson/static/css/main.f29438c1.chunk.css"
+  },
+  {
+    "revision": "7f0b5bdeb85b660321c884d863ac397f",
+    "url": "/portifolioAnderson/index.html"
+  }
+];
